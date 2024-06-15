c209b60eaef36cb58179165c640a672117931b4a
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-15 05:55:45 +0000

    modify

--- streamWeb/index5.py
+++ streamWeb/index5.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.bar_chart({"data":[1,5,2,6,2,1]})
+
+with st.expander(":orange-background[看說明]",expanded=True):
+    st.markdown('''
+## Chart above
+- The chart above shows some numbers I picked for you.
+- I rolled actual dice for these, so they're **guaranteed** 
+                
+> to be random.            
+''')
+    st.image("https://static.streamlit.io/examples/dice.jpg")
+    
