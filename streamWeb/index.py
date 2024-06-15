beafb8023e10b0abbbddbb013f44a2a361c1f303
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-15 02:52:13 +0000

    modify

---
+++ streamWeb/index.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("這是一個測試")
+st.header("header")
+st.subheader("shbHeader")
+st.caption("caption")
+st.divider()
+st.markdown("##Hello! **_World_**")
+st.markdown("### 步驟1")
+st.markdown("### 步驟2")
