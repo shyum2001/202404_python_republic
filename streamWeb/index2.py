6227dfd85a3558030bb9c82a3747daf940753e45
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-15 03:16:22 +0000

    modify

---
+++ streamWeb/index2.py
@@ -0,0 +1,12 @@
+import streamlit as st
+col1, col2= st.columns([1,1]). #(3,1)前面就是3/4，後面就是1/4的欄位配置
+
+with col1:
+    st.title("這是欄位1")
+    st.header("欄位1header")
+    st.subheader("欄位1subHeader")
+
+with col2:
+    st.title("這是欄位2")
+    st.header("欄位2header")
+    st.subheader("欄位2subHeader")
