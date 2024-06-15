16aa000482611938f6f205cd3eeb3688a38d7a9f
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-15 07:37:55 +0000

    modify

---
+++ streamWeb/index13_1.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+if 'robert' not in st.session_state:
+    st.session_state['robert'] = 30
+
+st.session_state['robert'] #中括號為subscrip
+st.session_state.robert =50 #.為運算值
+st.session_state.robet
