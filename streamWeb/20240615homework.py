2d2856032764d413317e0dd81e8ad332ca717b66
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-21 16:30:38 +0000

    modify

---
+++ streamWeb/20240615homework.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("BMI值計算")
+st.subheader("BMI值計算公式: BMI = 體重(公斤) / 身高^2(公尺^2)")
+st.subheader("例如: 一個52公斤的人，身高是155公分，則BMI值為:")
+
+st.markdown('''
+            :blue["# 52(公斤)/1.552(公尺^2)=21.6 "])
+st.markdown('''
+            :red["## 體重正常範圍為BMI=18.5~24 "])
+
+
+
