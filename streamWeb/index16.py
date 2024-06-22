11aadb3a482e69d09bc035d0fdcc6b8848000fda
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-22 01:57:56 +0000

    modify

--- streamWeb/index16.py
+++ streamWeb/index16.py
@@ -2,10 +2,4 @@ import streamlit as st
 
 st.title("BMI值計算")
 st.subheader("BMI值計算公式: BMI = 體重(公斤) / 身高^2(公尺^2)")
-st.subheader("例如: 一個52公斤的人，身高是155公分，則BMI值為:")
-
-st.markdown('''
-            :blue["# 52(公斤)/1.552(公尺^2)=21.6 "])
-
-
-
+st.subheader("例如: 一個52公斤的人,身高是155公分,則BMI值為:")
