dee790969f3884bf67bc32056e9f5468eeaba6bf
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-22 05:40:32 +0000

    modify

--- lesson13/index.py
+++ lesson13/index.py
@@ -24,7 +24,7 @@ with st.form('bmi form',border=False):
 
     if st.form_submit_button("BMI計算"):
         bmi_result = round( weight / ((height/100) ** 2),1 )
-    weight = st.number_input(":green[選擇體重(kg)]",max_valee= 200,min_value=30)
+    weight = st.number_input(":green[選擇體重(kg)]",max_value= 200,min_value=30)
     if st.form_submit_button("BMI計算"):
         bmi_result = round( weight / ((height/100)**2),1)
 
