32fc598d4dbf29927e097d41c6a760e3913b485f
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-29 06:17:22 +0000

    modify

--- lesson13/index2.py
+++ lesson13/index2.py
@@ -1,9 +1,3 @@
-import requests
-from pydantic import BaseModel,Field,RootModel,field_validator,field_serializer
-import streamlit as st
-import source  #增加的地方
-from source import Root  #增加的地方
-
 
 try:
     data_str = source.download_youbike()
@@ -12,33 +6,22 @@ except Exception as e:
 else:
     root = Root.model_validate_json(data_str)
     data = root.model_dump()
-    areas: list[str] = list(set(map(lambda value: value['行政區'],data)))
-
-    #def area_change():
-    #    print("Hello")
-
-    #with st.sidebar:
-    #    st.selectbox(":orange[請選擇行政區域:]",options =areas,on_change=area_change,key='sarea')
-    #    st.session_state
-
-        #def search():
-        #    st.write("開始搜尋")
-
-        def area_change():
-            sarea_name = st.session_state.sarea
-            st.write(sarea_name)
-            display_data = []
-            for item in date:
-                if item[ '行政區'] == sarea_name:
-                    display_data.append(item)
-            st.write(display_data)
-        
-        with st.sidebar:
-            st.selectbox(":orange[請選擇行政區域:]", options =areas,on_change=area_change,key='sarea')
-
-        st.title("台北市youbike各行政區店點資料")
-
+    areas:list[str] = list(set(map(lambda value:value['行政區'],data)))
 
     
+    def area_change():
+        sarea_name = st.session_state.sarea
+        st.write(sarea_name)        
+        display_data = []
+        for item in data:
+            if item['行政區'] == sarea_name:
+                display_data.append(item)
+        st.write(display_data)
 
 
+
+    with st.sidebar:
+        st.selectbox(":orange[請選擇行政區域:]",options=areas,on_change=area_change,key='sarea')
+    
+    
+    st.title("台北市youbike各行政區站點資料")
