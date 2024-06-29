3aa403cc6e8dd7955525fab3be075bfa1f9cc284
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-29 06:37:28 +0000

    modify

--- lesson13/index_2_1.py
+++ lesson13/index_2_1.py
@@ -14,21 +14,20 @@ else:
     data = root.model_dump()
     areas:list[str] = list(set(map(lambda value:value['行政區'],data)))
 
-    st.title("台北市youbike各行政區站點資料")
-    tableContainer = st.container(height=500,border=False)
     
     def area_change():
         sarea_name = st.session_state.sarea
-        #st.write(sarea_name)        
+        st.write(sarea_name)        
         display_data = []
         for item in data:
             if item['行政區'] == sarea_name:
                 display_data.append(item)
-        with tableContainer:
-            st.subheader(sarea_name)
-            st.dataframe(data=display_data)
+        st.write(display_data)
 
 
 
     with st.sidebar:
         st.selectbox(":orange[請選擇行政區域:]",options=areas,on_change=area_change,key='sarea')
+    
+    
+    st.title("台北市youbike各行政區站點資料")
