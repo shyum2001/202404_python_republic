f865e24ac2f01d8e12231e1254fe3affbbda72d0
shyum2001 <168253202+shyum2001@users.noreply.github.com>
2024-06-29 06:14:26 +0000

    modify

--- lesson13/index2.py
+++ lesson13/index2.py
@@ -14,12 +14,12 @@ else:
     data = root.model_dump()
     areas: list[str] = list(set(map(lambda value: value['行政區'],data)))
 
-    def area_change():
-        print("Hello")
+    #def area_change():
+    #    print("Hello")
 
-    with st.sidebar:
-        st.selectbox(":orange[請選擇行政區域:]",options =areas,on_change=area_change,key='sarea')
-        st.session_state
+    #with st.sidebar:
+    #    st.selectbox(":orange[請選擇行政區域:]",options =areas,on_change=area_change,key='sarea')
+    #    st.session_state
 
         #def search():
         #    st.write("開始搜尋")
